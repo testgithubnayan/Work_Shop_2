9a188371e7d62b42929d8bf3f7d9a9b3ba6887ce
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-29 11:48:04 +0530

    Main clas for caling method and printing List

---
+++ HospitalMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class HospitalMainClass {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the hospital enquiry :--");
+		Hospital pc = new Hospital();
+		pc.patient();
+
+	}
+}
